549aa3210530067ff4a95c16f691c45fa79b1f38
Grey <GreyLee369@gmail.com>
2018-09-28 11:52:49 -0400

    update to adjust to local

--- ContentBasedRS.py
+++ ContentBasedRS.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel#, cosine_similarity
 
-meta_data = pd.read_csv('movies_metadata.csv')
-links = pd.read_csv('links.csv')
+meta_data = pd.read_csv('/Users/grey/Documents/Big Data/project/files/movies_metadata.csv',encoding='mac_roman')
+links = pd.read_csv('/Users/grey/Documents/Big Data/project/files/links_small.csv',encoding='mac_roman')
 links = links[links['tmdbId'].notnull()]['tmdbId'].astype('int')
 
 meta_data = meta_data.drop([4831,19730, 29503, 35587])
-meta_data = meta_data.drop_duplicates(['title '])
+meta_data = meta_data.drop_duplicates(['title'])
 meta_data['id'] = meta_data['id'].astype('int')
 
 lmd = meta_data[meta_data['id'].isin(links)]
@@ -16,10 +16,11 @@ lmd['description'] = lmd['description'].fillna('')
 
 tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
 tfidf_matrix = tf.fit_transform(lmd['description'])
+# print(tfidf_matrix)
 
 lmd = lmd.reset_index()
-titles = lmd['title ']
-indices = pd.Series(lmd.index, index=lmd['title '])
+titles = lmd['title']
+indices = pd.Series(lmd.index, index=lmd['title'])
 
 def get_recommendations(userProfile):
 	t = tfidf_matrix[indices[userProfile[0][0]]] * userProfile[0][1]
@@ -46,5 +47,5 @@ def print_res(userProfile):
 		if flag == 0:
 			print(res[i])
 
-userProfile = [['Spider ', 4.0], ['The Matrix Revolutions ', 3.0], ['Harry Potter and the Chamber of Secrets ', 5.0]]
+userProfile = [['Spider', 4.0], ['The Matrix Revolutions', 3.0], ['Harry Potter and the Chamber of Secrets', 5.0]]
 print_res(userProfile)
